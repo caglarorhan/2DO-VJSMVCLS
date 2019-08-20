984315e03f50f5c134701f915c68bee7c236efde
caglarorhan <caglaror@gmail.com>
2019-08-20 00:15:47 -0500

    Listing todos.

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ class Model {
         if(!localStorage.getItem('uid_const')){
             localStorage.setItem('uid_const','0');
         }
-            if(!localStorage.getItem('myToDos')) {
-                console.log('No todos!');
-                this.todos = [];
-                localStorage.setItem('myToDos', JSON.stringify(this.todos));
-            } else {
-                console.log('There are todos!');
-                this.todos = JSON.parse(localStorage.getItem('myToDos'));
-            }
+        if(!localStorage.getItem('myToDos')) {
+            console.log('No todos!');
+            this.todos = [];
+            localStorage.setItem('myToDos', JSON.stringify(this.todos));
+        } else {
+            console.log('There are todos!');
+            this.todos = JSON.parse(localStorage.getItem('myToDos'));
+        }
     }
     addToDo(todoObj){
         //
@@ -26,17 +26,22 @@ class Model {
         this.todos.push(todo);
         localStorage.setItem('myToDos', JSON.stringify(this.todos));
         localStorage.setItem('uid_const',parseInt(localStorage.getItem('uid_const'))+1);
+        return this.getToDos();
     }
 
     getToDos(){
-
+        this.todos = JSON.parse(localStorage.getItem('myToDos'));
+        return this.todos;
     }
+
+
+
 
     //delete todo
-    deleteToDo(){
+    deleteToDo(todoID){
     }
     // update todo
-    updateToDo(){
+    updateToDo(todoObj){
 
     }
 
@@ -45,6 +50,7 @@ class Model {
 
 class View {
     constructor() {
+        // header or name of todo
         let todoName =  document.createElement('input');
         let todoName_label = document.createElement('span');
             todoName_label.innerText = 'To Do Name:';
@@ -57,7 +63,7 @@ class View {
 
             //
             document.body.appendChild(document.createElement('br'));
-            //
+        // detail or information of todo
         let todoInfo =  document.createElement('textarea');
         let todoInfo_label = document.createElement('span');
             todoInfo_label.innerText = 'To Do Information:';
@@ -69,7 +75,7 @@ class View {
         //
         document.body.appendChild(document.createElement('br'));
         //
-
+        // todo adding button
         let addToDoButton = document.createElement('button');
             addToDoButton.id = 'add_todo_button';
             addToDoButton.type = 'button';
@@ -77,29 +83,75 @@ class View {
             document.body.appendChild(addToDoButton);
         //
         document.body.appendChild(document.createElement('br'));
+
+
         //
 
         this.todoName = todoName;
         this.todoInfo = todoInfo;
         this.addToDoButton = addToDoButton;
     }
+
+
     get _thisName(){
         return this.todoName.value;
     }
     get _thisInfo(){
         return this.todoInfo.value;
     }
-    bindAddToDo(handlerx){
-        this.addToDoButton.addEventListener('click',()=>{
-             handlerx({header:this._thisName, info:this._thisInfo});
+
+
+
+    bindAddToDo(handler){
+        this.addToDoButton.addEventListener('click', async ()=>{
+            let todoLists = await handler({header:this._thisName, info:this._thisInfo});
+            this.listToDos(todoLists);
+
         });
     }
+
+
+    listToDos(toDoList){
+        let listContainer;
+
+
+            if(!document.getElementById('list-container')){
+                let listContainerDiv = document.createElement('div');
+                listContainerDiv.id='list-container';
+                document.body.appendChild(listContainerDiv);
+            }
+
+        listContainer = document.getElementById('list-container');
+            listContainer.innerHTML='';
+
+        let todoUnOrderedListHeader = document.createElement('h3');
+        todoUnOrderedListHeader.innerText = 'ToDo List';
+        listContainer.appendChild(todoUnOrderedListHeader);
+        if(toDoList.length<1){
+            listContainer.innerHTML='There is no todo in the list!'
+        }else{
+            // todo list
+
+            let todoUnOrderedList = document.createElement('ul');
+            listContainer.appendChild(todoUnOrderedList);
+            toDoList.forEach((todo)=>{
+                let todoElm = document.createElement('li');
+                todoElm.innerHTML = `<input type="checkbox" ${todo.completed?"checked":''}> : <b>${todo.header}</b> <i>${todo.info} </i> <a href="void:0" onclick="alert('to be deleted')">DELETE</a>`;
+                todoUnOrderedList.appendChild(todoElm);
+            })
+        }
+    }
+
+
+
+
 }
 class Controller {
     constructor(model, view) {
         this.model = model;
         this.view = view;
-        this.view.bindAddToDo((tobj) => {this.model.addToDo(tobj)});
+        this.view.bindAddToDo((tobj) => { return this.model.addToDo(tobj)});
+        this.view.listToDos(this.model.getToDos());
     }
 
 
